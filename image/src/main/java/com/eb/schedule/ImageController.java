2bf279936390d9f52a63089ec19ec395c88f8f06
egoruch <Q1w2e3r4>
2016-06-15 23:07:22 +0300

    default image load image only once

--- image/src/main/java/com/eb/schedule/ImageController.java
+++ image/src/main/java/com/eb/schedule/ImageController.java
@@ -1,10 +1,14 @@
 package com.eb.schedule;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 /**
  * Created by Egor on 14.06.2016.
@@ -12,8 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class ImageController {
 
-    @RequestMapping("/image/{id}")
-    public byte[] index(@PathVariable("id") String id) {
-        return ImageUtils.getImage(id);
+    @Value("${imageFolder:f:/nginx-1.11.1/data/assets}")
+    String imageFolder;
+
+    private ImageUtils imageUtils;
+
+
+    @PostConstruct
+    public void init() throws IOException {
+        imageUtils = new ImageUtils(imageFolder);
+    }
+
+    @RequestMapping(path = "/image/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] image(@PathVariable("id") String id) {
+        return imageUtils.getImage(id);
     }
 }
--- image/src/main/java/com/eb/schedule/ImageUtils.java
+++ image/src/main/java/com/eb/schedule/ImageUtils.java
@@ -1,14 +1,19 @@
 package com.eb.schedule;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
 
 import java.io.*;
-import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Egor on 14.06.2016.
@@ -17,7 +22,40 @@ public class ImageUtils {
 
     private static final String GET_TEAM_LOGO = "http://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/?key=9EBD51CD27F27324F1554C53BEDA17C3&appid=570&ugcid=";
 
-    public static byte[] getImage(String id){
+    private final String dataFolder;
+    private final byte[] defaultImage;
+
+    private final LoadingCache<String, byte[]> images = CacheBuilder.newBuilder()
+            .maximumSize(100)
+            .expireAfterWrite(2, TimeUnit.MINUTES)
+            .build(
+                    new CacheLoader<String, byte[]>() {
+                        public byte[] load(String imageId) {
+                            return processImage(imageId);
+                        }
+                    });
+
+    public ImageUtils(String dataFolder) throws IOException {
+        this.dataFolder = dataFolder;
+        this.defaultImage = getDefaultImage(dataFolder);
+    }
+
+    private byte[] getDefaultImage(String dataFolder) throws IOException {
+        Path path = Paths.get(dataFolder).resolve("-1.png");
+        return Files.readAllBytes(path);
+    }
+
+    public byte[] getImage(String id) {
+        try {
+            return images.get(id);
+        } catch (ExecutionException e) {
+            // TODO logging
+            System.out.println(e.getMessage());
+        }
+        return defaultImage;
+    }
+
+    private byte[] processImage(String id) {
         try {
             HttpResponse<JsonNode> jsonResponse = Unirest.get(GET_TEAM_LOGO + id)
                     .header("accept", "application/json")
@@ -25,30 +63,28 @@ public class ImageUtils {
             JSONObject data = jsonResponse.getBody().getObject().getJSONObject("data");
             String url = data.getString("url");
 
-            try(InputStream inputStream = Unirest.get(url).asBinary().getBody()){
+            try (InputStream inputStream = Unirest.get(url).asBinary().getBody()) {
                 int read = 0;
                 byte[] bytes = new byte[1024];
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                    while ((read = inputStream.read(bytes)) != -1) {
+                        outputStream.write(bytes, 0, read);
+                    }
 
-                while ((read = inputStream.read(bytes)) != -1) {
-                    outputStream.write(bytes, 0, read);
+                    saveImage(outputStream, id);
+                    return outputStream.toByteArray();
                 }
-
-                saveImage(outputStream, id);
-                return outputStream.toByteArray();
             }
         } catch (Exception e) {
-            //todo default behaviour
-            e.printStackTrace();
+            // TODO logging
+            System.out.println(e.getMessage());
         }
-        return null;
+        return defaultImage;
     }
 
-    private static void saveImage(ByteArrayOutputStream outputStream, String id) throws FileNotFoundException {
-        try(FileOutputStream file = new FileOutputStream(new File("f:\\nginx-1.11.1\\data\\assets\\" +id + ".png"))){
+    private void saveImage(ByteArrayOutputStream outputStream, String id) throws Exception {
+        try (FileOutputStream file = new FileOutputStream(new File(dataFolder + id + ".png"))) {
             outputStream.writeTo(file);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
